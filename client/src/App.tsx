440fbd1b9fa4425d5b2b2cf4e3ddcd7d617c5c99
Stian Bakken <stian.bakken@cognite.com>
2021-06-01 16:34:03 +0200

    Fix error handling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,8 +7,12 @@ function App() {
   const [res, setRes] = useState<{ data: number[] } | null>(null);
 
   const fetchData = async (url: string) => {
-    const _res = await fetch(`${baseURL}${url}`).then((d) => d.json());
-    setRes(_res);
+    try {
+      const _res = await fetch(`${baseURL}${url}`).then((d) => d.json());
+      setRes(_res);
+    } catch (err) {
+      setRes(null);
+    }
   };
 
   return (
